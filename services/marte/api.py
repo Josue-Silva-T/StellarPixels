ba4b1626075e107ea68f477629c74e6e6e551b0c
urielxdov <urieledgar878@gmail.com>
2025-10-05 10:23:00 -0600

    ya esta

--- services/marte/api.py
+++ services/marte/api.py
@@ -21,7 +21,7 @@ def buscar_imagen(name: str):
     data = r.json()
     respuestas = data["response"]["docs"]
 
-    for respuesta in respuestas[0]:
+    for respuesta in respuestas:
         url = respuesta.get("ATLAS_DATA_URL") or ""
         name_photo = respuesta.get("FILE_NAME_SPECIFICATION", "").split("/")[-1]
 
@@ -38,6 +38,7 @@ def buscar_imagen(name: str):
         else:
             # opcional: si quieres seguir guardando las browse JPG/PNG
             convertir_jp2_url_a_tiff(url, name_photo)
+        break
 
 def guardar_memoria():
     pass
@@ -112,4 +113,4 @@ def convertir_jp2_url_a_tiff(url: str, tiff_name: str) -> Path:
     return tiff_path
 
 # Ejemplo:
-buscar_id("1_N1845993031.118")
+buscar_imagen("crater")
